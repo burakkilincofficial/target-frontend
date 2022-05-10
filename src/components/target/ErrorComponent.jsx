173ae0016c1fa13e0f1ea9f220fbfeebfdda4e22
Burak KILINC <burakkilincofficial@gmail.com>
2022-05-10 23:13:22 +0300

    initial commit

---
+++ src/components/target/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+function ErrorComponent() {
+    return (
+        <div>An Error Occurred.I dont why contact support burakilinc.7@gmail.com</div>
+    )
+}
+
+export default ErrorComponent
---
+++ src/components/target/LogoutComponent.jsx
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class LogoutComponent extends Component {
+
+    render() {
+        return (
+            <>
+                <h1>You are logged out</h1>
+                <div className="container"/>
+                Thank you for using Our Application
+            </>
+        )
+    }
+}
+
+export default LogoutComponent
