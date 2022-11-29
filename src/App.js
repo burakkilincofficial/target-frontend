91c7b060c926de3e325041fd56673913e012b41a
Burak KILINC <burakkilincofficial@gmail.com>
2022-11-29 10:24:43 +0300

    add tailwind configs update npm version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import TestComponent from "./components/TestComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome Target App</h1>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+                <p className="text-3xl text-gray-700 font-bold mb-5">
+                    Welcome!
+                </p>
+                <p className="text-gray-500 text-lg">
+                    React and Tailwind CSS in action
+                </p>
+            </div>
+            <div className="mt-10">
+                <TestComponent></TestComponent>
+            </div>
+            <div className="mt-5">
+                <TestComponent></TestComponent>
+            </div>
+        </>
+
+    );
 }
 
 export default App;
---
+++ src/components/TestComponent.js
@@ -0,0 +1,9 @@
+function TestComponent() {
+    return (
+        <h1 className="mt-10">
+            <p className="text-sky-400">test tailwind...</p>
+        </h1>
+    );
+}
+
+export default TestComponent;
